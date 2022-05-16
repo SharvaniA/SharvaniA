36b5e97538c75beb2b61f8a40e9ba6910372454d
SharvaniA <103307849+SharvaniA@users.noreply.github.com>
2022-05-16 13:27:11 +0530

    Add files via upload

---
+++ 5s.c
@@ -0,0 +1,28 @@
+//Reading and Printing n Names with pointers 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LENGTH 30
+int main()
+{
+	int names_count, names_counter;
+	printf("How many names do you want to print? ");
+	scanf("%d", &names_count);
+	fflush(stdin);
+	char **names;
+	names = malloc(names_count * sizeof(char * ));
+	for(names_counter = 0; names_counter < names_count; names_counter++)
+	{
+		names[names_counter] = malloc(MAX_LENGTH);
+	}
+	for(names_counter = 0; names_counter < names_count; names_counter++)
+	{
+		printf("Enter names %d of %d: ", names_counter + 1, names_count);
+		scanf("%s", names[names_counter]);
+	}
+	for(names_counter = 0; names_counter < names_count; names_counter++)
+	{
+		printf("Names %d of %d: %s\n", names_counter + 1, names_count, names[names_counter]);
+	}
+}
