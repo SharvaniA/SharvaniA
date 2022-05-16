56d64baefc4b5bdad3c2ae4169fba83adf729487
SharvaniA <103307849+SharvaniA@users.noreply.github.com>
2022-05-16 13:46:05 +0530

    Add files via upload

---
+++ dicVLC.c
@@ -0,0 +1,56 @@
+//Use a dictionary api to show the meaning of a given name and also play the pronunciation.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Tools.c"
+int main()
+{
+	char wordToSearch[20];
+	char urlCommandLine[200];
+	char vlcCommandLine[200];
+	char data[1000];
+	char *partOfData;
+	printf("Enter word to search: ");
+	fgets(wordToSearch, 20, stdin);
+	remove_newline(wordToSearch);
+	sprintf(urlCommandLine, "curl -k -s \"https://api.dictionaryapi.dev/api/v2/entries/en/%s\" > meaning.dat\n", wordToSearch);
+	system(urlCommandLine);
+
+	FILE *fpCurlDicFile;
+
+	fpCurlDicFile = fopen("meaning.dat", "r");
+	fgets(data, sizeof(data), fpCurlDicFile);
+	// fwrite(data, 900, 1, fpCurlDicFile);
+	// fclose(fpCurlDicFile);
+
+	// fpCurlDicFile = fopen("meaning.dat", "r");
+	// fread(data, sizeof(data), 1, fpCurlDicFile);
+	partOfData = strtok(data, ":[],{};");
+	while (partOfData != NULL)
+	{
+		//printf("%s\n", partOfData);
+		partOfData = strtok(NULL, ":[],{};");
+		if(strstr(partOfData, "\"definition\"") != NULL)
+		{
+			printf("Definition: ");
+			partOfData = strtok(NULL, ":[],{};");
+			printf("%s\n", partOfData);
+			break;
+		}
+		else if(strstr(partOfData, "\"audio\"") != NULL)
+		{
+			partOfData = strtok(NULL, "[],{}");
+			sprintf(vlcCommandLine, "vlc %s\n", partOfData);
+			system(vlcCommandLine);
+		}
+		// else if(strstr(partOfData, "\"meaning\"") != NULL)
+		// {
+		// 	printf("Meaning: ");
+		// 	partOfData = strtok(NULL, ":[],{};");
+		// 	printf("%s\n", partOfData);
+		// 	break;
+		// }
+	}
+	fclose(fpCurlDicFile);
+}
